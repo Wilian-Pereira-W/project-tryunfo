5e26414b15bd2d4646284571c722cf3c95c9a9f4
Wilian Pereira <wilian_pereira92@outlook.com>
2021-10-19 20:35:50 -0300

    Adicionado requisito 5

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.onInputChange = this.onInputChange.bind(this);
+    this.validarCamposFomulario = this.validarCamposFomulario.bind(this);
 
     this.state = {
       cardName: '',
@@ -16,7 +17,7 @@ class App extends React.Component {
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
 
@@ -24,11 +25,40 @@ class App extends React.Component {
     const { id, type } = target;
     const value = type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({ [id]: value });
+    this.setState({ [id]: value }, this.validarCamposFomulario);
   }
 
   onSaveButtonClick() {
 
+  }
+
+  validarCamposFomulario() {
+    const { cardName, cardDescription, cardImage, cardRare,
+      cardAttr1, cardAttr2, cardAttr3,
+    } = this.state;
+    const camposDigitados = [cardName, cardDescription, cardImage, cardRare];
+    const camposDeNumeros = [Number(cardAttr1), Number(cardAttr2), Number(cardAttr3)];
+    const valorMaxAtributo = 90;
+    const valorMaxSoma = 210;
+    const somaDoCamposDeNumeros = Number(cardAttr1) + Number(cardAttr2)
+     + Number(cardAttr3);
+    const validarTamanhoCampo = camposDigitados.every((texto) => texto.length > 0);
+    const validarAtributos = camposDeNumeros.every((numero) => numero >= 0
+     && numero <= valorMaxAtributo);
+    const validarSomaDosAtributos = somaDoCamposDeNumeros <= valorMaxSoma;
+
+    const ValidarTodosInputs = validarTamanhoCampo && validarAtributos
+    && validarSomaDosAtributos;
+
+    if (ValidarTodosInputs === true) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   }
 
   render() {
