0ad7dd671e78bf945b8f87b6d2e394d4797d084d
Wilian Pereira <wilian_pereira92@outlook.com>
2021-10-19 21:47:39 -0300

    Reajustado requisito 6

--- src/App.js
+++ src/App.js
@@ -57,7 +57,6 @@ class App extends React.Component {
       cardAttr2: '0',
       cardAttr3: '0',
       cardRare: 'normal',
-      hasTrunfo: cards.some((card) => card.cardTrunfo),
     });
   }
 
